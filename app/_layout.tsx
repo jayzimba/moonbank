010e54e0c9acb79bc77c42bc9016a7a2cc3e473e
mac <jayzimba40@gmail.com>
2024-11-01 09:19:59 +0200

    changes made

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -27,7 +27,7 @@ export default function RootLayout() {
 
   return (
     <Stack>
-      <Stack.Screen name="index" options={{ headerShown: true }} />
+      <Stack.Screen name="index" options={{ headerShown: false }} />
       <Stack.Screen name="login" options={{ headerShown: false }} />
       <Stack.Screen name="signup" options={{ headerShown: false }} />
       <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
--- app/login.tsx
+++ app/login.tsx
@@ -1,3 +1,5 @@
+
+
 import {
   Button,
   ImageBackground,
@@ -5,12 +7,17 @@ import {
   StyleSheet,
   Text,
   View,
+  TextInput,
 } from "react-native";
 import React from "react";
 import ButtonComponent from "@/components/ButtonComponent";
 import { router } from "expo-router";
 import { Colors } from "@/constants/Colors";
-import { FontAwesome6 } from "@expo/vector-icons";
+import {
+  Feather,
+  FontAwesome6,
+  MaterialCommunityIcons,
+} from "@expo/vector-icons";
 
 export default function index() {
   return (
@@ -36,13 +43,41 @@ export default function index() {
         </Text>
       </View>
 
+      <View style={styles.TextInputStyle}>
+        <MaterialCommunityIcons name="email-outline" size={24} color="grey" />
+        <TextInput
+          placeholder="example@gmail.com"
+          keyboardType="email-address"
+          autoCapitalize="none"
+          selectionColor={"grey"}
+          style={{
+            color: "grey",
+            width: "90%",
+            paddingHorizontal: 10,
+          }}
+        />
+      </View>
+      <View style={styles.TextInputStyle}>
+        <Feather name="lock" size={24} color="grey" />
+        <TextInput
+          keyboardType="default"
+          autoCapitalize="none"
+          secureTextEntry={true}
+          inputMode="text"
+          selectionColor={"grey"}
+          style={{
+            color: "grey",
+            width: "90%",
+            paddingHorizontal: 10,
+          }}
+        />
+      </View>
+
       <ButtonComponent
         text="Login"
         bordered={false}
         onPress={() => router.navigate("/(tabs)/home")}
       />
-
-     
     </ImageBackground>
   );
 }
@@ -54,5 +89,17 @@ const styles = StyleSheet.create({
     alignItems: "center",
     justifyContent: "center",
     padding: 20,
+  },
+  TextInputStyle: {
+    width: "100%",
+    height: 50,
+    borderColor: Colors.light.primary,
+    borderWidth: 1,
+    marginBottom: 20,
+    paddingHorizontal: 15,
+    borderRadius: 50,
+    alignItems: "center",
+    flexDirection: "row",
+    gap: 10,
   },
 });
